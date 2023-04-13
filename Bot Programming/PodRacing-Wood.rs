1fe03807d36443b8221ba1df32d3d4e637abc81f
Katherine <kstedman.engr@gmail.com>
2023-04-13 12:34:41 -0600

    Pod racing bot programming

---
+++ Bot Programming/PodRacing-Wood.rs
@@ -0,0 +1,38 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let x = parse_input!(inputs[0], i32);
+        let y = parse_input!(inputs[1], i32);
+        let next_checkpoint_x = parse_input!(inputs[2], i32); // x position of the next check point
+        let next_checkpoint_y = parse_input!(inputs[3], i32); // y position of the next check point
+        let next_checkpoint_dist = parse_input!(inputs[4], i32); // distance to the next checkpoint
+        let next_checkpoint_angle = parse_input!(inputs[5], i32); // angle between your pod orientation and the direction of the next checkpoint
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let opponent_x = parse_input!(inputs[0], i32);
+        let opponent_y = parse_input!(inputs[1], i32);
+        let mut thrust: usize = 0;
+        
+        if next_checkpoint_angle > 90 || next_checkpoint_angle < -90 {
+            thrust = 0;
+        } else {
+            thrust = 100;
+        }
+        
+        println!("{} {} {}", next_checkpoint_x, next_checkpoint_y,thrust);
+    }
+}
